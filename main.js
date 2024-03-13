9affcc64c41b2d70b373ebe76987c5947874ea8d
Dionna <businessladythick@yahoo.com>
2024-03-12 22:16:46 -0400

    added initial files

---
+++ main.js
@@ -0,0 +1,14 @@
+//in order to use the variable, porperties, functuality, n a module you hav to import it
+// import { name, getValue} from './modules/modFile.js'
+// console.log('this is what im doing');
+// 
+// console.log(name);
+// console.log(getValue(6));
+
+// import { name, getValue} from './modules/modFile.js'
+// console.log(name);
+// console.log(getValue(19));
+
+import { data, getValue} from'./modules/modFile.js'
+console.log(data);
+console.log(getValue(99));
---
+++ modules/modFile.js
@@ -0,0 +1,24 @@
+// export const name = "Module File Value"  //this line will export line 3
+// 
+// export function getValue(arg) {  //this line will return line 5
+    //seeing what this will do something eith arg
+    // return('this is passing in the value' + arg);
+// }
+
+
+console.log('---------------------------')
+
+
+// export const name = "I think I have this figured out"
+
+// export function getValue(arg) {
+    // return('this lins should read WHOA' + arg);
+// }
+
+
+export const data = "this is the one"
+
+export function getValue(atl) {
+    return('dionna is the man' + atl)
+}
+
